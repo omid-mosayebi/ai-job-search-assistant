98b00b31369e90dbe5b9a240352648208916e84e
Omid Mosayebi <omidmosayebi@gmail.com>
2026-07-05 06:28:14 +0000

    Add SQLAlchemy base and user model

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
-
 from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=True
+)
 
 SessionLocal = sessionmaker(
-    autoflush=False,
-    autocommit=False,
     bind=engine,
+    autocommit=False,
+    autoflush=False,
 )
